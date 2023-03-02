5059004e9dfe9c2bc8d1397f0da2ac4994014f8f
Jesus <jesus.cassano.2006@gmail.com>
2023-03-02 17:55:00 +0100

    🐛 Add user file fix changes

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,5 +1,22 @@
+import { Beer } from './beer';
+
 export type User = {
   id: string;
   email: string;
   password: string;
+  beers: Beer[];
 };
+
+/**
+ 1 - n  User -> n // Beer -> 1
+ n - m  User -> n // Beer -> n
+Relacion
+
+
+OTRA MANERA
+UserBeer = {
+  userID
+  beerID
+}
+
+ */
